a42ddd60eed462765f6fe98ddac1363c47a58da6
Takeshi Kimura <kittykimura@gmail.com>
2019-05-13 13:57:42 +0000

    add jbat.py and JBATS_1.0.

---
+++ jbat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import vecto.embeddings
+import vecto.benchmarks.analogy.analogy
+import pickle
+
+path_model = "./embeddings-jawiki/"
+model = vecto.embeddings.load_from_dir(path_model)
+options = {}
+options["path_dataset"] = "./JBATS_1.0/"
+ana = vecto.benchmarks.analogy.analogy.Analogy()
+d = ana.run(model, options["path_dataset"])
+with open('./result.pickle', 'w') as pf:
+    pickle.dump(d, pf, protocol=4)
+
