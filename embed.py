0008e0a485366d61c5288f54b8bed6d7298225d8
Takeshi KIMURA <kittykimura@gmail.com>
2019-06-28 17:50:02 +0900

    remove snapshot move commands.

--- embed.py
+++ embed.py
@@ -215,7 +215,7 @@ def main():
 
         while not logQ.empty():
             lmsg, pth = logQ.get()
-            shutil.move(pth, opt.checkpoint)
+            # shutil.move(pth, opt.checkpoint)
             log.info(f'json_stats: {json.dumps(lmsg)}')
 
     control.checkpoint = True
