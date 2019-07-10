65db86379ad88930a5dec424215916ac2dfc5ea2
Takeshi Kimura <kittykimura@gmail.com>
2019-07-10 08:47:50 +0000

    now training parameters...

--- train-table1.py
+++ train-table1.py
@@ -5,9 +5,23 @@ import sys
 import subprocess
 
 pcall_base = ['python3', 'embed.py']
-pcall_base_options_dict = {'-epochs': '1500',
+# pcall_base_options_dict = {'-epochs': '500',
+#                            '-negs': '50',
+#                            '-dampening': '0.75',
+#                            '-ndproc': '4',
+#                            '-eval_each': '1',
+#                            '-fresh': '',
+#                            '-sparse': '',
+#                            '-gpu': '0',
+#                            '-burnin_multiplier': '0.01',
+#                            '-neg_multiplier': '0.1',
+#                            '-lr_type': 'constant',
+#                            '-train_threads': '1',
+#                            '-dampening':  '1.0',
+#                            '-batchsize': '10'}
+
+pcall_base_options_dict = {'-epochs': '1000',
                            '-negs': '50',
-                           '-dampening': '0.75',
                            '-ndproc': '4',
                            '-eval_each': '100',
                            '-fresh': '',
@@ -18,8 +32,8 @@ pcall_base_options_dict = {'-epochs': '1500',
                            '-lr_type': 'constant',
                            '-train_threads': '1',
                            '-dampening':  '1.0',
-                           '-batchsize': '50'}
-
+                           '-batchsize': '10'}
+# pcall_option_poincare = {'-lr': '0.3'}
 pcall_option_poincare = {'-lr': '1.0'}
 pcall_option_lorentz = {'-lr': '0.5', '-no-maxnorm': ''}
 # pcall_option_lorentz = {'-lr': '0.5'}
@@ -52,11 +66,13 @@ def main():
     # manifolds = ['euclidean', 'poincare', 'lorentz']
     # dimensions = [5, 10, 20, 50, 100, 200]
     manifolds = ['poincare']
-    dimensions = [10]
-    dataset = './wordnet/noun_closure.csv'
+    # manifolds = ['lorentz']
+    dimensions = [20]
+    # dataset = './wordnet/noun_closure_synsets_jp.csv'
+    dataset = './wordnet/noun_closure_jp.csv'
     for mani in manifolds:
         for dim in dimensions:
-            outfile = './newemb1500/emb_%s_%d.bin' % (mani, dim)
+            outfile = './pojwords20/emb_%s_%d.bin' % (mani, dim)
             cmd_python3 = make_command_poincare(
                 manifold=mani, dim=str(dim), dset=dataset, checkpoint=outfile)
             print(cmd_python3)
